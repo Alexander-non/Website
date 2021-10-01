8582bea2f2c0bbc15f2201c886239b86a477d627
Alexander-non <69676176+Alexander-non@users.noreply.github.com>
2021-10-01 23:33:54 +0200

    1.0.9

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById("menu-button-open").onclick = function menu() {
+    document.getElementById("menu-sidepage").style.display = "block";
+}
+document.getElementById("menu-button-close").onclick = function menu() {
+    document.getElementById("menu-sidepage").style.display = "none";
+}
