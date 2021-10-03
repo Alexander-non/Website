62c0e2121783fdeba8ea7323ec9065413cae9d7c
Alexander-non <69676176+Alexander-non@users.noreply.github.com>
2021-10-03 21:37:27 +0200

    1.0.9.2
    On my way to correct and create a menu bar for my webpage.

--- index.js
+++ index.js
@@ -1,6 +1,67 @@
-document.getElementById("menu-button-open").onclick = function menu() {
-    document.getElementById("menu-sidepage").style.display = "block";
+var langClicked = 0;
+var themeClicked = 0;
+var pressed = 0;
+
+document.getElementById("menu-button-open").onclick = function menuOpen() {
+    pressed++;
+    document.getElementById("menu-sidepage").style.width = "25%";
+    document.getElementById("menu-button-close").style.display = "block";
+    document.getElementById("menu-sidepage-buttons-holder").style.display = "block";
+    document.getElementById("blackscreen").style.display = "block";
 }
-document.getElementById("menu-button-close").onclick = function menu() {
-    document.getElementById("menu-sidepage").style.display = "none";
+document.getElementById("menu-button-close").onclick = function menuClose() {
+    pressed--;
+    document.getElementById("menu-sidepage").style.width = "0%";
+    document.getElementById("menu-sidepage-buttons-holder").style.display = "none";
+    document.getElementById("menu-button-close").style.display = "none";
+    document.getElementById("blackscreen").style.display = "none";
+}
+document.addEventListener("keydown", function (event) {
+    if (event.key === 'm') {
+        pressed++;
+    if (pressed % 2 == 0) {
+        document.getElementById("menu-sidepage").style.width = "0%";
+        document.getElementById("menu-sidepage-buttons-holder").style.display = "none";
+        document.getElementById("menu-button-close").style.display = "none";
+        document.getElementById("blackscreen").style.display = "none";
+    } else {
+        document.getElementById("menu-sidepage").style.width = "25%";
+        document.getElementById("menu-button-close").style.display = "block";
+        document.getElementById("menu-sidepage-buttons-holder").style.display = "block";
+        document.getElementById("blackscreen").style.display = "block";
+        }
+    }
+});
+
+
+document.getElementById("language").onclick = function languageChange() {
+    langClicked++;
+
+    if (langClicked % 2 == 0) {
+        document.getElementById("language-select-holder").style.display = "none";
+    } else {
+        document.getElementById("language-select-holder").style.display = "block";
+    }
+}
+document.getElementById("theme").onclick = function menu() {
+    themeClicked++;
+
+    if (themeClicked % 2 == 0) {
+        document.getElementById("theme-select-holder").style.display = "none";
+    } else {
+        document.getElementById("theme-select-holder").style.display = "block";
+    }
+}
+
+
+document.getElementById("language-select").onclick = function languageChange() {
+    key = document.getElementById("language-select").innerHTML
+    switch (key) {
+        case "Angol":
+            document.getElementById("szorakozas").innerHTML == "ENTERTAINMENT";
+            break;
+    
+        default:
+            break;
+    }
 }
