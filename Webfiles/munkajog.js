ba0ef91ef1b81f5a9fd1de74a7cd9cec9141d867
Alexander-non <69676176+Alexander-non@users.noreply.github.com>
2021-12-05 20:22:44 +0100

    1.0.9.5

--- Webfiles/munkajog.js
+++ Webfiles/munkajog.js
@@ -1,8 +1,19 @@
+var x = 0
 function fogalom() {
-    document.getElementById("probaido").innerHTML = '<b> Próda idő: </b> A munkaszerződésben kikötött, általában 30 napos időtartam, amely alatt a munkaviszonyt bármelyik fél azonnali hatállyal megszüntetheti.';
-    document.getElementById("gyakornok").innerHTML = '<b> Gyakornok: </b> Gyakornok az a kezdő alkalmazott, aki szakképesítése, illetve oklevele megszerzése után a hivatásra való felkészülés közben már szakmai tevékenységet folytat'
-    document.getElementById("felmondasi-ido").innerHTML = `<b> Felmondási idő: </b> Felmondás esetén hacsak nem azonnali hatályú felmondásról van szó, melynek szigorú törvényi követelményei vannak nem egyik napról a másikra tud <br> lelépni a dolgozó, hanem van egy bizonyos felmondási idő, amelyet le kell töltenie.`
-    document.getElementById("beteg-szabadsag").innerHTML = '<b> Betegszabadság: </b> A beteg szabadság ami a munkáltatónak fizetedő, amikor valamilyen betegség vagy sérülés miatt képtelen dolgozni'
-    document.getElementById("cafeteria").innerHTML = '<b> Cafetéria: </b> Cafetéria vagy másnéven választható juttatási rendszer az egy bizonyos keretenbelül megszabottben levő juttatásokat kérhetsz.'
-    document.getElementById("kollektiv-szerzodes").innerHTML = '<b> Kollektív szerződés: </b> A munkáltató és a dolgozók között (általában szakszervezet által) létrejött külön megállapodás a foglalkoztatással kapcsolatban.'
-}
+    x++;
+    if (x % 2 == 0) {
+        document.getElementById("probaido").innerHTML = "";
+        document.getElementById("gyakornok").innerHTML = "";
+        document.getElementById("felmondasi-ido").innerHTML = "";
+        document.getElementById("beteg-szabadsag").innerHTML = "";
+        document.getElementById("cafeteria").innerHTML = "";
+        document.getElementById("kollektiv-szerzodes").innerHTML = "";
+    } else {
+        document.getElementById("probaido").innerHTML = '<b> Próda idő: </b> A munkaszerződésben kikötött, általában 30 napos időtartam, amely alatt a munkaviszonyt bármelyik fél azonnali hatállyal megszüntetheti.';
+        document.getElementById("gyakornok").innerHTML = '<b> Gyakornok: </b> Gyakornok az a kezdő alkalmazott, aki szakképesítése, illetve oklevele megszerzése után a hivatásra való felkészülés közben már szakmai tevékenységet folytat'
+        document.getElementById("felmondasi-ido").innerHTML = `<b> Felmondási idő: </b> Felmondás esetén hacsak nem azonnali hatályú felmondásról van szó, melynek szigorú törvényi követelményei vannak nem egyik napról a másikra tud <br> lelépni a dolgozó, hanem van egy bizonyos felmondási idő, amelyet le kell töltenie.`
+        document.getElementById("beteg-szabadsag").innerHTML = '<b> Betegszabadság: </b> A beteg szabadság ami a munkáltatónak fizetedő, amikor valamilyen betegség vagy sérülés miatt képtelen dolgozni'
+        document.getElementById("cafeteria").innerHTML = '<b> Cafetéria: </b> Cafetéria vagy másnéven választható juttatási rendszer az egy bizonyos keretenbelül megszabottben levő juttatásokat kérhetsz.'
+        document.getElementById("kollektiv-szerzodes").innerHTML = '<b> Kollektív szerződés: </b> A munkáltató és a dolgozók között (általában szakszervezet által) létrejött külön megállapodás a foglalkoztatással kapcsolatban.'
+    }
+};
