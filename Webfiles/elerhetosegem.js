f5821ad139386af94431990793fc7452087235ae
Alexander-non <69676176+Alexander-non@users.noreply.github.com>
2021-12-23 16:53:37 +0100

    1.0.9.8

--- Webfiles/elerhetosegem.js
+++ Webfiles/elerhetosegem.js
@@ -1,6 +1,6 @@
 function MouseOver(currentPictureID, changedPictureSRC) {
   changedPicture = '../Pictures/' + changedPictureSRC
-  document.getElementById(currentPictureID).src = document.getElementById(currentPictureID).src.replace("Pictures/", ("../Pictures/" + changedPictureSRC))
+  document.getElementById(currentPictureID).src = document.getElementById(currentPictureID).src.replace("Pictures/", changedPicture)
 }
 function MouseOut(currentPicture, keyword) {
     document.getElementById(currentPicture).src= '../Pictures/' + keyword + '.png';
