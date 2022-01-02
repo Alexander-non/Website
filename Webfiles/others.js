7a329afe0c54756008b708bd64ac038aeedab8a7
Alexander-non <69676176+Alexander-non@users.noreply.github.com>
2022-01-02 19:54:30 +0100

    1.0.9.9.5.3

--- Webfiles/others.js
+++ Webfiles/others.js
@@ -1,5 +1,4 @@
-done = false;
-
+var done = false;
 function Rotate(spin) {
     element = document.getElementById(spin);
     done = true;
@@ -12,6 +11,31 @@ function Rotate(spin) {
                 element.style.animationName = "none";
                 location.reload()
             }
-        }, 1000);
+        }, 500);
+    }
+}
+
+function Switch(element) {
+    target = document.getElementById(element);
+    others = ["time-table", "calculator", "clock"]
+    switch (element) {
+        case others[0]:
+            target.style.visibility = "visible";
+            document.getElementById(others[1]).style.visibility = "hidden";
+            document.getElementById(others[2]).style.visibility = "hidden";
+            break;
+        case others[1]:
+            target.style.visibility = "visible";
+            document.getElementById(others[0]).style.visibility = "hidden";
+            document.getElementById(others[2]).style.visibility = "hidden";
+            break;
+        case others[2]:
+            target.style.visibility = "visible";
+            document.getElementById(others[0]).style.visibility = "hidden";
+            document.getElementById(others[1]).style.visibility = "hidden";
+            break;
+        
+        default:
+            break;
     }
 }
